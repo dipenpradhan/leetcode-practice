e4edcfe99224c2119dbdcbf30092d5b2cbff3480
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-07 20:15:09 -0500

    Time: 1 ms (42.00%), Space: 38.2 MB (72.17%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,30 @@
+class Solution {
+    public double myPow(double x, int n) {
+       
+        
+        if(n == 0) return 1;
+        else if(x == 0) return 0;
+        
+        
+        double result = 1;
+        double currVal = x;
+        long pow = n;
+        if(pow<0) pow = -pow;
+        
+        if(n < 0) currVal = 1/x;
+        
+        while(pow > 0){
+            
+            if(pow%2==1){
+                result *= currVal;    
+            }
+            
+            currVal *= currVal;
+            pow /= 2;
+            
+        }
+        
+        return result;
+          
+    }
+}
