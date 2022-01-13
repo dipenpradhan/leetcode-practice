a410af0361caeb999a9577d01e6da79216469edb
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-12 20:18:42 -0500

    Time: 4 ms (62.62%), Space: 39.4 MB (54.89%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,60 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        /* 
+            fn combsum(candidates, target)
+                    result = [][]
+                    currList = []
+                    backtrack(candidates, 0, result, currList,target)
+                    
+        
+            fn backtrack(candidates, start, chosen, remaining)
+                if start exceeds candidates - STOP
+                if remaining = 0 - STOP, add this chosen list to result
+                
+                
+                loop over candidates, starting from "start"
+                    add candidate on chosen list
+                    // try using same candidate again
+                    // update remaining value
+
+                    backtrack(candidates, start, result, chosen, target-chosen)
+
+                    remove candidate from chosen list
+                    
+        */
+        
+        
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> chosen = new ArrayList<Integer>();
+        
+        backtrack(candidates, 0, result, chosen, target);
+        
+        return result;
+    }
+    
+    
+    private void backtrack(int[] candidates, int start, List<List<Integer>> result, List<Integer> chosen, int remaining){
+        
+        if(start >= candidates.length || remaining < 0) return;
+        else if(remaining == 0) {
+            List<Integer> resultItem = new ArrayList<Integer>();
+            resultItem.addAll(chosen);
+            result.add(resultItem);
+            return;
+        }
+        
+        for(int i = start; i < candidates.length; i++){
+            
+            // if(candidates[i] <= remaining){
+            chosen.add(candidates[i]);
+            backtrack(candidates, i, result, chosen, remaining - candidates[i]);
+            chosen.remove(chosen.size()-1);
+            // }else{
+                // backtrack(candidates, start+1, result, chosen, remaining);
+            // }
+            
+        }
+        
+    }
+}
