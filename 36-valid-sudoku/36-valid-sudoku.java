476511fcea3f4b8f5a783ad42b7ecf39fcf560ef
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-14 18:48:13 -0500

    Time: 10 ms (32.86%), Space: 39.7 MB (47.99%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -0,0 +1,83 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+        /*
+            fn valid(board):
+                seeBox map<r|c, hashSet>
+                loop over rows r:
+                    seenRows hashSet
+                    seenCols hashSet
+                    loop over cols c:
+                        check if value each cell r,c is present in seenRows
+                        check if value each cell c,r is present in seenCols
+                        check if value each cell r,c is present in r/3 | c/3 in seenBox
+                        exit false if any of above happens
+                exit true otherwise
+        */
+        
+        if( board == null || board.length != 9 || board[0].length != 9) return false;
+        
+        int N = 9;
+        Map<String, Set<Character>> boxSeen = new HashMap<>();
+        
+        for(int i = 0; i < N; i ++){
+            Set<Character> rowSeen = new HashSet<>();
+            Set<Character> colSeen = new HashSet<>();
+            
+            for(int j = 0; j < N; j ++){
+                
+                char rowChar = board[i][j];
+                char colChar = board[j][i];
+                
+                if(rowChar != '.'){
+                    if(rowSeen.contains(rowChar)){
+                        return false;
+                    }else{
+                        rowSeen.add(rowChar);
+                    }
+                    
+                    int boxRowIx = i/3;
+                    int boxColIx = j/3;
+                    String boxKey = boxRowIx + "|" + boxColIx;
+                    
+                    if(boxSeen.containsKey(boxKey)){
+                        if(boxSeen.get(boxKey).contains(rowChar)){
+                            return false;
+                        }else{
+                            boxSeen.get(boxKey).add(rowChar);
+                        }
+                    }else{
+                        boxSeen.put(boxKey, new HashSet<Character>());
+                        boxSeen.get(boxKey).add(rowChar);
+                    }
+                }
+                
+                if(colChar != '.'){
+                    if(colSeen.contains(colChar)){
+                        return false;
+                    }else{
+                        colSeen.add(colChar);
+                    }
+                }
+                
+                
+            }
+        }
+        
+                    return true;
+
+    }
+    
+    
+    
+}
+
+// [".",".",".",".","5",".",".","1","."],
+// [".","4",".","3",".",".",".",".","."],
+// [".",".",".",".",".","3",".",".","1"],
+// ["8",".",".",".",".",".",".","2","."],
+// [".",".","2",".","7",".",".",".","."],
+// [".","1","5",".",".",".",".",".","."],
+// [".",".",".",".",".","2",".",".","."],
+// [".","2",".","9",".",".",".",".","."],
+// [".",".","4",".",".",".",".",".","."]
