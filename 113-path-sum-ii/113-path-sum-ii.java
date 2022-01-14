4b198629433d3378144152b853cb4c64d4004b7d
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-13 20:09:24 -0500

    Time: 2 ms (47.69%), Space: 41.6 MB (22.46%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        
+        /*
+            
+            fn pathSum(root, target):
+                result = [][]
+                currentPath = []
+                backtrack(root, result, currentPath, target)
+                
+            fn backtrack(node, result, currentPath, remaining):
+                
+                if node is null STOP
+                if remaining is 0, add currentPath to result, STOP
+                if remaining < 0 STOP
+                
+                add node to currentPath
+                backtrack(node.left, result, currentPath, remaining - node.val)
+                backtrack(node.right, result, currentPath, remaining - node.val)
+                remove node from currentPath
+                
+        */
+        
+        
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> currentPath = new ArrayList<>();
+        
+        backtrack(root, result, currentPath, targetSum);
+        return result;
+    }
+    
+    private void backtrack(TreeNode node, List<List<Integer>> result, List<Integer> currentPath, int remaining){
+        // if(remaining < 0) return;
+        if(node == null) return;
+        
+        currentPath.add(node.val);
+
+        if(node.left == null && node.right == null && remaining == node.val) {
+            List<Integer> resultItem = new ArrayList<Integer>();
+            resultItem.addAll(currentPath);
+            result.add(resultItem); 
+        }else{
+            backtrack(node.left, result, currentPath, remaining - node.val);
+            backtrack(node.right, result, currentPath, remaining - node.val);   
+        }
+        
+        currentPath.remove(currentPath.size()-1);
+    }
+}
