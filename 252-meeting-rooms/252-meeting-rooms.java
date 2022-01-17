005a63222e64b38d4187da1273300fe2e568411b
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-17 16:08:52 -0500

    Time: 4 ms (97.64%), Space: 39 MB (51.92%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.java
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -7,16 +7,19 @@ class Solution {
         // Sort by start time
         Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
         
-        
-        // Check for overlap
         for(int i = 0; i < intervals.length - 1; i++){
             
             int[] interval = intervals[i];
             int[] nextInterval = intervals[i+1];
             
-            if (interval == null || interval.length != 2) return false;
-            if (nextInterval == null || nextInterval.length != 2) return false;
+            // Protect against invalid intervals
+            if (interval == null || interval.length != 2 || interval[1] < interval[0])
+                return false;
             
+            if (nextInterval == null || nextInterval.length != 2 || nextInterval[1] < nextInterval[0])
+                return false;
+            
+            // Check for overlap
             if(nextInterval[0] < interval[1]){
                 return false;
             }
