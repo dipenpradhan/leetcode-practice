15f8e7a29894c78b9b85bae13c52bd63fc0fa2fd
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-17 00:23:25 -0500

    Time: 17 ms (97.58%), Space: 72.3 MB (77.89%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -18,13 +18,39 @@ class Solution {
     /*
         DFS
         
-        result
-        answerFound
-        countUp
-        constants FOUND_START, FOUND_DEST, NONE
+        string result
+        bool ansFound to stop recursion
+        int countUp for recording up moves
+        constants FOUND_NONE, FOUND_START, FOUND_DEST, FOUND_BOTH
+        
         fn int dfs(node, start, dest, down):
+            code = FOUND_NODE
+            if node is null or ansFound:
+                exit FOUND_NONE
             
+            if node is start:
+                set code to FOUND_START
             
+            if node is end:
+                set code to FOUND_END
+                
+            L = dfs(left,...
+            R = dfs(right,...
+            
+            if code + L + R = FOUND_BOTH:
+                ansFound = true
+            
+            if L is FOUND_START OR R is FOUND_START:
+                upCount++
+                code = FOUND_START
+            
+            if L is FOUND_END OR R is FOUND_END:
+                append L OR R to down
+                code = FOUND_END
+            
+            return code
+            
+        result = append upCount number of U, append reverse of down string because our recursion returns the path upside down
     */
     
     private StringBuilder result = new StringBuilder();
@@ -77,8 +103,7 @@ class Solution {
             down.append('R');
             code = FOUND_DEST;
         }
-        
-        
+ 
         return answerFound ? -1 : code;
     }
     
