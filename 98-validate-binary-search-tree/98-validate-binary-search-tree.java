c82fe5be584829e358eb547b20aaa27465b9ef87
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-16 21:23:35 -0500

    Time: 0 ms (100.00%), Space: 40.9 MB (30.20%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    /*
+        Using recursion, check if left subtree is valid and right subtree is valid
+        Validity of subtree in BST:
+            - all nodes in left subtree are less than parent
+            - all nodes in right subtree are greater than parent
+            
+        fn isValidRecurse(node, min, max):
+            if node is less than min or greater than max:
+                FAIL
+            if isValidRecurse(left, min, node) and isValidRecurse(right, node, max) are true:
+                SUCCESS
+            else
+                FAIL
+    */
+    public boolean isValidBST(TreeNode root) {
+           
+        return validBSTRecursive(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        
+    }
+    
+    
+    private boolean validBSTRecursive(TreeNode node, long minVal, long maxVal){
+        
+        if(node == null) return true;
+        
+        if(node.val <= minVal || node.val >= maxVal){
+            return false;
+        }
+        
+        if(validBSTRecursive(node.left, minVal, node.val) 
+           && validBSTRecursive(node.right, node.val, maxVal)
+           ){
+            return true;
+        }
+        if(node.left == null && node.right == null){
+            return true;
+        }
+        return false;
+    }
+}
