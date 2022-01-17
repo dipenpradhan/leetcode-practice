0a9341d343e4ac98837d03c1aad57d976bf48e26
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-16 21:24:41 -0500

    Time: 0 ms (100.00%), Space: 41 MB (30.20%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -20,7 +20,7 @@ class Solution {
         Validity of subtree in BST:
             - all nodes in left subtree are less than parent
             - all nodes in right subtree are greater than parent
-            
+        Use long datatype to protect against integer max and min value existing in tree    
         fn isValidRecurse(node, min, max):
             if node is less than min or greater than max:
                 FAIL
