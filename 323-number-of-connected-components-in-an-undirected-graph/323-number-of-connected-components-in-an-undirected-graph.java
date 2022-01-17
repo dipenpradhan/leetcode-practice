513f3bdfaa8767b9d1ef41d2ca513f1d06f3ce7a
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-17 01:18:09 -0500

    Time: 30 ms (5.12%), Space: 52.4 MB (5.12%) - LeetHub

---
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.java
@@ -0,0 +1,50 @@
+class Solution {
+    
+    private int count = 0;
+    
+    public int countComponents(int n, int[][] edges) {
+        // System.out.println("edges="+edges);
+        boolean[] visited = new boolean[n];
+        
+        Map<Integer, List<Integer>> adjList = new HashMap<Integer, List<Integer>>();
+        
+        for(int[] edge: edges){
+            if(!adjList.containsKey(edge[0])){
+                adjList.put(edge[0], new ArrayList<Integer>());
+            }
+            adjList.get(edge[0]).add(edge[1]);
+            if(!adjList.containsKey(edge[1])){
+                adjList.put(edge[1], new ArrayList<Integer>());
+            }
+            adjList.get(edge[1]).add(edge[0]);
+        }
+        System.out.println("adjList = "+adjList );
+
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                dfs(i, visited, n, adjList);
+                count++;
+            }
+            
+        }
+        
+        return count;
+        
+    }
+    
+    private void dfs(int node, boolean[] visited, int n, Map<Integer, List<Integer>> adjList){
+        
+        if(!visited[node]){
+            visited[node] = true;
+            
+            if(adjList.containsKey(node)){
+                for(int j : adjList.get(node)){
+                    dfs(j, visited, n, adjList);
+                }
+            }
+            
+        }
+        
+        
+    }
+}
