2cde7944fed07b388fe1548cc41bb6204c7ba17e
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-17 21:00:40 -0500

    Time: 11 ms (96.23%), Space: 38.5 MB (73.40%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+        //         0  1  2  3  4  5  6  7  8  9  10  11 
+        //     1 | 0  1  2  3  4  5  6  7  8  9  10  11 
+        //   1,2 | 0  1  1  2  2  3  3  4  4  5  5   6  
+        // 1,2,5 | 0  1  1  2  2  1  2  2  3  3  2   3  
+      
+        //
+        //
+        //
+        //
+        //
+        
+        int[] memo = new int[amount+1];
+        
+        for(int i = 0; i < memo.length; i++){
+            if(i == 0){
+                memo[0] = 0;
+            }else{
+                memo[i] = amount + 1;
+            }
+            
+        }
+        
+        
+        for(int a = 1; a <= amount; a++){
+            for(int c = 0; c < coins.length; c++){
+                
+                int coin = coins[c];
+                
+                if(a >= coin){
+                    memo[a] = Math.min(memo[a], memo[a - coin] + 1);
+                }
+                
+            }
+            
+            
+        }
+        
+        return memo[amount] > amount ? -1 : memo[amount];
+        
+    }
+}
