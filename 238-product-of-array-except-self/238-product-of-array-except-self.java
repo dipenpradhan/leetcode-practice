383784c1ab0f81dd59f43003cde3e375bf35a7b0
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-20 17:02:28 -0500

    Time: 2 ms (57.60%), Space: 52.3 MB (54.96%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -3,15 +3,17 @@ class Solution {
         // [1,2,3,4]
         // [24,12,8,6]
         int N = nums.length;
-        int[] pf = new int[N];
-        int[] sf = new int[N];
-        pf[0] = 1;
-        sf[N-1] = 1;
+        
+        int[] pf = new int[N]; // prefix product
+        int[] sf = new int[N]; // suffix product
+        
+        pf[0] = 1; // prefix at start of array is [], so set pf product to 1
+        sf[N-1] = 1; // suffix at end of array is [], so set sf product to 1
         
         for(int i = 1, j = N-2; i < N; i++, j--){
             
-            pf[i] = nums[i-1] * pf[i-1];
-            sf[j] = nums[j+1] * sf[j+1];
+            pf[i] = nums[i-1] * pf[i-1]; // at every index, calculate product for i from start until i-1 (prefix)
+            sf[j] = nums[j+1] * sf[j+1]; // at every index, calculate product for j from end until j+1 (suffix)
 
         }
         // System.out.println("pf="+Arrays.toString(pf));
@@ -19,9 +21,7 @@ class Solution {
         int[] result = new int[N];
         
         for (int i = 0; i < N; i++){
-            
-            result[i] = pf[i] * sf[i];
-            
+            result[i] = pf[i] * sf[i]; // result is product of prefix and suffix at position "i"
         }
         
         return result;
