c14b3281a748214778e12ef56b0d01cbe846c1de
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-20 17:25:44 -0500

    Time: 5 ms (11.15%), Space: 56.6 MB (42.21%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,27 +1,54 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // [1,2,3,4]
-        // [24,12,8,6]
-        int N = nums.length;
+        // [1,2,3,4]  // input
+        // [24,12,8,6] // expected result
         
-        int[] pf = new int[N]; // prefix product
-        int[] sf = new int[N]; // suffix product
-        
-        pf[0] = 1; // prefix at start of array is [], so set pf product to 1
-        sf[N-1] = 1; // suffix at end of array is [], so set sf product to 1
-        
-        for(int i = 1, j = N-2; i < N; i++, j--){
+        /*
+            product except self:
+                product of everything behind current number and everything after current number
+                prefix * suffix
+                prefix = product of all numbers before current
+                suffix = product of all numbers after current
+                
+            N = nums length
+            pf = [N] //prefix
+            sf = [N] // suffix
             
-            pf[i] = nums[i-1] * pf[i-1]; // at every index, calculate product for i from start until i-1 (prefix)
-            sf[j] = nums[j+1] * sf[j+1]; // at every index, calculate product for j from end until j+1 (suffix)
-
-        }
-        // System.out.println("pf="+Arrays.toString(pf));
-        // System.out.println("sf="+Arrays.toString(sf));
+            
+            [1,2,3,4]
+            prefix list:                                              suffix list:
+                nums[0]: 1   pf: [],      product = 1    pf[0] = 1    sf: [2,3,4], product = 24   sf[0] = 24
+                nums[1]: 2   pf: [1],     product = 1    pf[1] = 1    sf: [3,4],   product = 12   sf[1] = 12
+                nums[2]: 3   pf: [1,2],   product = 2    pf[2] = 2    sf: [4],     product = 4    sf[2] = 4
+                nums[3]: 4   pf: [1,2,3], product = 6    pf[3] = 6    sf: [],      product = 1    sf[3] = 1
+                
+            populate prefix and suffix
+            populate result:
+                result = prefix[i] * suffix[i]
+                result = [1*24, 1*12, 2*4, 6*1]
+                result = [24, 12, 8, 6]
+                
+        
+        */
+        
+        
+        int N = nums.length;
+        int[] pf = new int[N];
+        int[] sf = new int[N];
         int[] result = new int[N];
         
-        for (int i = 0; i < N; i++){
-            result[i] = pf[i] * sf[i]; // result is product of prefix and suffix at position "i"
+        for(int i = 0, j = N-1; i < N; i++, j--){
+            
+            if(i == 0){
+                pf[i] = 1; // empty set prefix [] product is 1
+                sf[j] = 1; // empty set suffix [] product is 1
+            }else{
+                pf[i] = nums[i-1] * pf[i-1]; // product of previous number and the prefix product at that index
+                sf[j] = nums[j+1] * sf[j+1]; // product of next number and the suffix product at that index
+            }
+        }
+        for(int i = 0; i < N; i++){
+            result[i] = pf[i] * sf[i]; // Result is product of prefix and suffix
         }
         
         return result;
