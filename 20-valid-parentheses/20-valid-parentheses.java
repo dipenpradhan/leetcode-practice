252a1b50699ee0fced10181e0d2a83f7462fbd78
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-20 23:26:02 -0500

    Time: 2 ms (55.74%), Space: 38.9 MB (23.85%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,30 +1,53 @@
 class Solution {
     
-    Map<Character, Character> map;
-    
-    Solution(){
-        map = new HashMap<Character, Character>();
-        map.put('}','{');
-        map.put(']','[');
-        map.put(')','(');    
-    }
-    public boolean isValid(String s) {
+//     Map<Character, Character> map;
+//     
+//     Solution(){
+//         map = new HashMap<Character, Character>();
+//         map.put('}','{');
+//         map.put(']','[');
+//         map.put(')','(');    
+//     }
+//     public boolean isValid(String s) {
 
-        Stack<Character> stk = new Stack<Character>();
+//         Stack<Character> stk = new Stack<Character>();
         
-        for(char c : s.toCharArray()){
+//         for(char c : s.toCharArray()){
             
-            if(!map.containsKey(c)){
-                stk.push(c);
-            }else if(stk.isEmpty() || stk.peek() != map.get(c)){
-                return false;
-            }else{
-                stk.pop();
+//             if(!map.containsKey(c)){
+//                 stk.push(c);
+//             }else if(stk.isEmpty() || stk.peek() != map.get(c)){
+//                 return false;
+//             }else{
+//                 stk.pop();
+//             }
+            
+//         }
+        
+//         return stk.isEmpty();
+        
+//     }
+    
+        public boolean isValid(String s) {
+            Stack<Character> stk = new Stack();
+            
+            for(char c : s.toCharArray()){
+                
+                if(c == '{' || c == '(' || c == '['){
+                    stk.push(c);
+                }else{
+                    if(stk.isEmpty()) return false;
+                    
+                    char p = stk.pop();
+                    if(c == '}' && p != '{') return false;
+                    if(c == ')' && p != '(') return false;
+                    if(c == ']' && p != '[') return false;
+                }
+                
             }
             
+            return stk.isEmpty();
+            
         }
-        
-        return stk.isEmpty();
-        
-    }
+
 }
