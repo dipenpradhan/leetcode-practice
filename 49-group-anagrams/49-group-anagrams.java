0dcae058518220cf61b6fab959050324b59f0314
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-23 20:37:10 -0500

    Time: 11 ms (60.33%), Space: 42 MB (78.97%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        
+        for(String s : strs){
+            
+            int[] a = new int[26];
+            for(char c : s.toCharArray()){
+                a[c-'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < a.length; i++){
+                char c = (char)('a'+i);
+                if(a[i] > 0){
+                    sb.append(c);
+                    sb.append(':');
+                    sb.append(a[i]);
+                    sb.append('|');
+
+                }
+            }
+            if(sb.length() > 0){
+                sb.setLength(sb.length()-1);    
+            }
+            String cstr = sb.toString();
+            if(!map.containsKey(cstr)){
+                map.put(cstr, new ArrayList<String>());
+            }
+            map.get(cstr).add(s);
+        }
+        // System.out.println(map);
+        return new ArrayList<List<String>>(map.values());
+    }
+}
