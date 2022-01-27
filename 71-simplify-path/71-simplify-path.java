28214ce707d2cbd65222e481b52a858067fea2fb
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-27 00:11:24 -0500

    Time: 10 ms (29.85%), Space: 44.4 MB (5.89%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String simplifyPath(String path) {
+        ArrayDeque<String> q = new ArrayDeque<String>();
+        
+        StringBuilder p = new StringBuilder();
+            
+        for(int i = 0; i < path.length(); i ++){
+            
+            char c = path.charAt(i);
+            if(c!='/'){
+                p.append(c);
+            }
+            if((c == '/' || i == path.length()-1) && p.length() > 0){
+                
+                if(p.toString().equals("..")){
+                    if(!q.isEmpty()) q.pollLast();
+                }else if(!p.toString().equals(".")){
+                    q.offer(p.toString());
+                }
+                p.setLength(0);
+                
+            }
+            
+        }
+        StringBuilder res = new StringBuilder();
+        res.append("/");
+        while(!q.isEmpty()){
+            res.append(q.poll());
+            res.append("/");
+        }
+        if(res.length() > 1){
+            res.setLength(res.length()-1);
+        }
+        return res.toString();
+    }
+}
