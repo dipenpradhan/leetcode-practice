117c4e87caafcdc14d2d6a2e256496e1aa92a33d
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-29 17:34:21 -0500

    Time: 8 ms (57.51%), Space: 54.5 MB (5.92%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -8,14 +8,11 @@ class Solution {
     
     private boolean isValidPalindrome(String s, int l, int r, boolean del){
         while(l<=r){
-            // System.out.println("L "+l+" "+s.charAt(l)+" R "+r+" "+s.charAt(r));
             if(s.charAt(l)==s.charAt(r)){
                 l++; r--;
             }else if (!del){
-                // System.out.println("Setting del true");
                 return isValidPalindrome(s, l, r-1, true) || isValidPalindrome(s, l+1, r, true);
             }else{
-                // System.out.println("returning false, del="+del);
                 return false;
             }
         }
@@ -23,4 +20,3 @@ class Solution {
     }
     
 }
-// "aguokepatgbnvfqmgmlc u puufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuc u lmgmqfvnbgtapekouga"
