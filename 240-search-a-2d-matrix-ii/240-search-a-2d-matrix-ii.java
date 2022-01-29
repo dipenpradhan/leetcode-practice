9c5becb737b8b9c2a31909897db55821254cda3b
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-29 03:12:14 -0500

    Time: 4 ms (100.00%), Space: 48.2 MB (50.30%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -4,64 +4,24 @@ class Solution {
         ROWS = matrix.length;
         COLS = matrix[0].length;
         
-        int[] row = findRow(matrix, target);
-        if(row[0]<0 && row[1]<0)return false;
-        row[0] = row[0]<0?matrix.length-1:row[0];
-        row[1] = row[1]<0?0:row[1];
-        // System.out.println(Arrays.toString(row));
-
-        for(int r = row[1]; r <= row[0]; r++){
-            if(findCol(matrix, r, target)>=0){
-                return true;
+        
+        return search(matrix, target);
+        
+    }
+    private boolean search(int[][] mat, int t){
+        int r = ROWS - 1, c = 0;
+        
+        while(r >= 0 && c < COLS){
+            if(t == mat[r][c]) return true;
+            else if(t > mat[r][c]){
+                c++;
+            }else{
+                r--;
             }
         }
+        
+        
         return false;
-        
-    }
-    private int findCol(int[][] matrix, int row, int target){
-        
-        int l = 0, r = matrix[row].length-1;
-        
-        while(l<=r){
-            int m = l + (r-l)/2;
-            
-            if(matrix[row][m] == target){
-                return m;
-            }else if(matrix[row][m]<target){
-                l = m + 1;
-            }else{
-                r = m - 1;
-            }
-        }
-        return -1;
-        
-    }
-    private int[] findRow(int[][] mat, int t){
-        
-        int less = -1, great = -1;
-        
-        int s = 0, e = ROWS-1;
-        
-        while(s <= e){
-            int m = s + (e-s)/2;
-            int c = less < 0 ? 0 : COLS - 1;
-            
-            if(mat[m][c] <= t && (m==e || mat[m+1][c] > t)){
-                if(less < 0){
-                    less = m;
-                    e = m;
-                }else{
-                    great = m;
-                    return new int[]{less, great};
-                }
-            }else if(mat[m][c] > t){
-                e = m-1;
-            }else{
-                s = m+1;
-            }    
-            
-        }
-        return new int[]{less, great};
     }
     
 }
