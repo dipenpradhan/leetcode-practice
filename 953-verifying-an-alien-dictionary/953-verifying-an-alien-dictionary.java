aa50ddf4d1ad70260bfc60b0ea691f5cf449b4cb
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-28 21:35:09 -0500

    Time: 8 ms (6.09%), Space: 43.5 MB (5.61%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
@@ -0,0 +1,36 @@
+class Solution {
+    
+    public boolean isAlienSorted(String[] words, String order) {
+        
+        int[] alpha = new int[26];
+        
+        for(int i = 0; i < order.length(); i++){
+            char c = order.charAt(i);
+            alpha[c-'a'] = i;
+        }
+        
+        
+        for(int i = 0; i < words.length - 1; i++){
+            String w1 = words[i];
+            String w2 = words[i+1];
+            
+            int j = 0;
+            while(j < w1.length() && j < w2.length() && w1.charAt(j) == w2.charAt(j) ){
+                
+                j++;
+            }
+            
+            if(w1.length() > w2.length() && j >= w2.length()){
+                System.out.println("Fst"+j);
+                return false;
+            }else if(j < w1.length() && j < w2.length() && alpha[w1.charAt(j)-'a'] > alpha[w2.charAt(j)-'a']){
+                System.out.println("Sec");
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+    
+}
