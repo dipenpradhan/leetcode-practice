0283bc40e2ec1a002820fe25c63b7bae7e8e2831
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-29 00:33:14 -0500

    Time: 0 ms (100.00%), Space: 42.8 MB (8.38%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        if(matrix == null) return false;
+        
+        int row = findRow(matrix, target);
+        return findCol(matrix, row, target)!=-1;
+    }
+    
+    
+    
+    private int findRow(int[][] mat, int t){
+        
+        int s = 0, e = mat.length-1;
+        
+        while(s <= e){
+            int m = s + (e-s)/2;
+            
+            if(mat[m][0] == t){
+                return m;
+            }else if(mat[m][0] < t && (m == mat.length-1 || mat[m+1][0] > t)){
+                return m;
+            }else if(mat[m][0] < t){
+                s = m+1;
+            }else{
+                e = m-1;
+            }
+        }
+        
+        return -1;
+        
+    }
+    
+    
+    private int findCol(int[][] mat, int row, int t){
+        
+        if(row < 0 || row >= mat.length) return -1;
+        
+        int l = 0, r = mat[row].length-1;
+        
+        while(l<=r){
+            int m = l + (r-l)/2;
+            
+            if(mat[row][m] == t) return m;
+            else if(mat[row][m] < t){
+                l = m + 1;
+            }else{
+                r = m - 1;
+            }
+        }
+        return -1;
+    }
+}
