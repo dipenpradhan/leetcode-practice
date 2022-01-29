15464a3fd85757b3a86c8cd41c31743bebf41351
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-29 17:48:48 -0500

    Time: 0 ms (100.00%), Space: 42.3 MB (48.70%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.java
+++ 766-toeplitz-matrix/766-toeplitz-matrix.java
@@ -5,25 +5,12 @@ class Solution {
         int COLS = mat[0].length;
         
         for(int r = 0; r < ROWS; r++){
-            int c = 0;
-            int n = mat[r][c];
-            int rNxt = r;
-            
-            while(rNxt < ROWS-1 && c < COLS-1){
-                if(mat[++rNxt][++c]!=n) return false;
+            for(int c = 0; c < COLS; c++){
+                int n = mat[r][c];
+                if(r+1 < ROWS && c+1 < COLS && mat[r+1][c+1]!=n) return false;
             }
-            
         }
         
-        for(int c = 0; c < COLS; c++){
-            int r = 0;
-            int n = mat[r][c];
-            int cNxt = c;
-            
-            while(r < ROWS-1 && cNxt < COLS-1){
-                if(mat[++r][++cNxt]!=n) return false;
-            }
-        }
         return true;
         
     }
