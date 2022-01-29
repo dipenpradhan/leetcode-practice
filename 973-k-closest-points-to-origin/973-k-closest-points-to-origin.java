36ef399064ffc689d9dfe0aaf8212caa3033a746
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-28 21:47:48 -0500

    Time: 29 ms (53.27%), Space: 50.7 MB (41.24%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -6,26 +6,17 @@ class Solution {
         // x = points[0], y = points[1]
         //  sqrt((x)^2 + (y)^2)
         
-        PriorityQueue<int[]> heap = new PriorityQueue<int[]>(
-            (p1, p2)->{
+        PriorityQueue<int[]> heap = new PriorityQueue<int[]>((p1, p2)->{
                 int x1 = p1[0], y1 = p1[1];
                 int x2 = p2[0], y2 = p2[1];
-                
                 double diff = Math.sqrt(x1*x1 + y1*y1) - Math.sqrt(x2*x2 + y2*y2);
-                if(diff == 0){
-                    return 0;
-                }else if(diff > 0){
-                    return 1;
-                }else{
-                    return  -1;    
-                }
-                
-            }
-            );
+                return diff>0 ? 1: diff<0 ? -1:0;
+            });
         
         for(int[] point : points){ 
             heap.add(point);
         }
+        
         for(int i = 0; i < k; i++){
             int[] p = heap.poll();
             res[i][0] = p[0];
