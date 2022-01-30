d194af460d119be5c80315263badbd251b43b976
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-30 18:20:54 -0500

    Time: 1 ms (98.22%), Space: 40.4 MB (5.39%) - LeetHub

---
+++ 408-valid-word-abbreviation/408-valid-word-abbreviation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        
+        int a = 0, w = 0;
+        StringBuilder buf = new StringBuilder();
+        
+        while(a < abbr.length() || w < word.length()){
+            
+            if(a<abbr.length() && Character.isDigit(abbr.charAt(a))){
+                buf.append(abbr.charAt(a));
+                a++;
+            }else if(buf.length()>0){
+                if(buf.charAt(0)=='0') return false;
+                w += Integer.parseInt(buf.toString());
+                buf.setLength(0);
+            }else if( a < abbr.length() && w < word.length() && abbr.charAt(a) == word.charAt(w)){
+                a++;
+                w++;
+            }else{
+                return false;
+            }
+            
+        }
+        return !(buf.length()>0 || a != abbr.length() || w != word.length());
+        
+    }
+}
