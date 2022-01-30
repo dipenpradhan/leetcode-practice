786557b9873f2bc3225e83abb724969ed5e2777a
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-29 20:25:48 -0500

    Time: 3 ms (63.70%), Space: 43.6 MB (8.16%) - LeetHub

--- 415-add-strings/415-add-strings.java
+++ 415-add-strings/415-add-strings.java
@@ -1,26 +1,28 @@
 class Solution {
     public String addStrings(String fst, String sec) {
         
-        return addNum(parseNum(fst),parseNum(sec));
+        return addNum(fst,sec);
     }
     
-    private String addNum(List<Integer> fst, List<Integer> sec){
-        int f = fst.size()-1, s = sec.size()-1;
+    private String addNum(String fst, String sec){
+        int f = fst.length()-1, s = sec.length()-1;
         StringBuilder numStr = new StringBuilder();
 
         int c = 0;
         while(f>=0 || s>=0){
+            int fn = f>=0?Character.getNumericValue(fst.charAt(f)):-1;
+            int sn = s>=0?Character.getNumericValue(sec.charAt(s)):-1;
             
             if(f>=0 && s>=0){
-                int r = (fst.get(f)+sec.get(s)+c)%10;
+                int r = (fn+sn+c)%10;
                 numStr.append(r);
-                c = (fst.get(f)+sec.get(s)+c)/10;
+                c = (fn+sn+c)/10;
             }else if (f>=0){
-                numStr.append((fst.get(f)+c)%10);
-                c = (fst.get(f)+c)/10;
+                numStr.append((fn+c)%10);
+                c = (fn+c)/10;
             }else{
-                numStr.append((sec.get(s)+c)%10);
-                c = (sec.get(s)+c)/10;
+                numStr.append((sn+c)%10);
+                c = (sn+c)/10;
             }
             f--;
             s--;
@@ -30,18 +32,7 @@ class Solution {
         
         return numStr.reverse().toString();
     }
-    
-    private List<Integer> parseNum(String s){
-        List<Integer> num = new ArrayList<Integer>();
-        
-        for(int i = 0; i < s.length(); i++){
-            int n = Character.getNumericValue(s.charAt(i));
-            num.add(n);
-        }
-        
-        return num;
-        
-    }
+  
 }
 
 //"3876620623801494171"
