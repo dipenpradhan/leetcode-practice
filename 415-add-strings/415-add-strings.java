d2138e14a669652f8ad8051107d47e30804f73ee
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-29 20:21:36 -0500

    Time: 5 ms (37.08%), Space: 43.8 MB (8.05%) - LeetHub

--- 415-add-strings/415-add-strings.java
+++ 415-add-strings/415-add-strings.java
@@ -6,32 +6,29 @@ class Solution {
     
     private String addNum(List<Integer> fst, List<Integer> sec){
         int f = fst.size()-1, s = sec.size()-1;
-        List<Integer> res = new ArrayList<Integer>();
+        StringBuilder numStr = new StringBuilder();
+
         int c = 0;
         while(f>=0 || s>=0){
             
             if(f>=0 && s>=0){
                 int r = (fst.get(f)+sec.get(s)+c)%10;
-                res.add(r);
+                numStr.append(r);
                 c = (fst.get(f)+sec.get(s)+c)/10;
             }else if (f>=0){
-                res.add((fst.get(f)+c)%10);
+                numStr.append((fst.get(f)+c)%10);
                 c = (fst.get(f)+c)/10;
             }else{
-                res.add((sec.get(s)+c)%10);
+                numStr.append((sec.get(s)+c)%10);
                 c = (sec.get(s)+c)/10;
             }
             f--;
             s--;
 
         }
-        StringBuilder numStr = new StringBuilder();
         if(c>0)numStr.append(c);
-        for(int i = res.size()-1; i>=0; i--){
-            numStr.append(res.get(i));
-        }
         
-        return numStr.toString();
+        return numStr.reverse().toString();
     }
     
     private List<Integer> parseNum(String s){
