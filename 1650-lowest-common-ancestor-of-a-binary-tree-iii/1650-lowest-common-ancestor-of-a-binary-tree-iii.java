f547753f4cd449ba9652dbb1b5fed966fb2364a8
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-31 01:33:09 -0500

    Time: 75 ms (5.66%), Space: 48.5 MB (5.65%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -11,10 +11,6 @@ class Node {
 class Solution {
     public Node lowestCommonAncestor(Node p, Node q) {
         
-        // Node rp = dfs(p, q);
-        // if(rp!=null) return rp;
-        // Node rq = dfs(q, p);
-        // if(rq!=null) return rq;
         Node pt = p, qt = q;
         
         Set<Node> set = new HashSet<Node>();
@@ -33,18 +29,5 @@ class Solution {
         }
         return null;
     }
-    
-    private Node dfs(Node n, Node s){
-        if(n==null || s == null) return null;
-        
-        if(n == s) return n;
-        
-        Node l = dfs(n.left, s);
-        if(l!=null) return l;
-        Node r = dfs(n.right, s);
-        if(r!=null) return r;
-        
-        return null;
-        
-    }
+ 
 }
