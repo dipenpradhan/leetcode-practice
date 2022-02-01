2791d127f9f292676e9c0fe8121e1259fc33d037
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-01-31 20:01:34 -0500

    Time: 0 ms (100.00%), Space: 40 MB (5.59%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.java
+++ 228-summary-ranges/228-summary-ranges.java
@@ -2,15 +2,20 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> res = new ArrayList<String>();
         int l = 0, r = 0;
-        
+        StringBuilder buf = new StringBuilder();
         while(r<nums.length){
             
             if(r == nums.length-1 || (r<nums.length-1 && nums[r] + 1 != nums[r+1])){
+                buf.setLength(0);
                 if(l==r){
-                    res.add(""+nums[l]);
+                    buf.append("");
+                    buf.append(nums[l]);
                 }else{
-                    res.add(nums[l]+"->"+nums[r]);
+                    buf.append(nums[l]);
+                    buf.append("->");
+                    buf.append(nums[r]);
                 }
+                res.add(buf.toString());
                 l=r;
                 l++;
             }
