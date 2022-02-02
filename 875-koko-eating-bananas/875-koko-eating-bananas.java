e5c43130983ef9245820d9665e99376a7d1f4dad
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-02-02 18:09:04 -0500

    Time: 27 ms (58.84%), Space: 43.5 MB (72.02%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -0,0 +1,52 @@
+class Solution {
+    private static final int MIN=0, MAX=1, TOT=2;
+    public int minEatingSpeed(int[] piles, int h) {
+        int[] arr = minMaxTot(piles);
+        
+        return binS(piles, h, 1, arr[MAX]);
+    }
+    
+    private int binS(int[] piles, int h, int l, int r){
+        
+        while(l<=r){
+            int m = l + (r-l)/2;
+            // System.out.println("l="+l+" m="+m+" r="+r);
+            boolean mBan = eatBans(piles, m, h);
+            if(mBan){
+                r = m-1;
+            }else{
+                l = m+1;
+            }
+        }
+        return l;
+    }
+    
+    private boolean eatBans(int[] pls, int max, int hrs){
+        
+        int t = 0;
+        for(int p: pls){
+            t+=p/max;
+            t+=(p%max)>0?1:0;
+        }
+        
+        System.out.println("t="+t);
+        return t <= hrs;
+    }
+    private int[] minMaxTot(int[] p){
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, tot=0; 
+        for(int b:p){
+            min = Math.min(min, b);
+            max = Math.max(max, b);
+            tot+=b;
+        }
+        return new int[]{min, max, tot};
+    }
+}
+
+/*
+
+[30,11,23,4,20]
+5
+[3,6,7,11]
+8
+*/
