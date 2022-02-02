b587a5b7041b35fa566f05de737adb99a89d931f
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-02-01 20:55:32 -0500

    Time: 30 ms (13.56%), Space: 40.3 MB (5.74%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -16,31 +16,16 @@ public class Solution extends VersionControl {
             
             mid = left + (right - left) / 2;
             
-            // check if mid is bad version
-            // if it is a bad version, we will check if the previous version was also bad
             if(isBadVersion(mid)){
-                
-                // if previous version was not bad, and we are not looking at the first version then mid is the first bad version
-                if(mid != left && !isBadVersion(mid-1)){
-                    return mid;
-                }else if(mid == left){
-                    // if we are looking at the first version then first is the first bad version
-                    return left;
-                }else{
-                    // if we find that previous version was also bad then we search to the left of mid
-                    // because first bad version will be somewhere before the current mid we are looking at
-                    right = mid - 1;
-                }
-                
+
+                right = mid - 1;
             }else{
-                // if we find a version that is not bad at mid, it implies the first bad version has not yet happened
-                // so we continue our search to the right of the mid
+
                 left = mid + 1;
             }
-            
-            
+
         }
-        return -1;
+        return left;
     }
     
 }
