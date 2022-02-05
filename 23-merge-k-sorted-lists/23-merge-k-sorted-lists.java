e2f3440d60f9b5f25321938ea8f40e85ab2d7b8e
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-02-05 18:57:21 -0500

    Time: 9 ms (44.07%), Space: 47 MB (12.03%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -25,44 +25,4 @@ class Solution {
          return res.next;
     }
     
-//     public ListNode mergeKLists(ListNode[] lists) {
-//         ListNode[] ptList = buildPtList( lists);
-//         ListNode res = new ListNode(-1);
-//         ListNode curr = res;
-        
-//         while(true){
-//             ListNode n = findMin(ptList);
-//             if(n==null) return res.next;
-//             curr.next = n;
-//             curr = curr.next;
-//         }
-        
-//     }
-    
-    private ListNode findMin(ListNode[] ptList){
-        int min = Integer.MAX_VALUE, m = -1;
-        for(int i = 0; i < ptList.length; i++){
-            ListNode pt = ptList[i];
-            if(pt!=null && pt.val<=min){
-                m = i;
-                min = pt.val;
-            }
-        }
-        if(m==-1) return null;
-        
-        ListNode minNode = ptList[m];
-        ptList[m] = ptList[m].next;
-
-        return minNode;
-    }
-    
-    private ListNode[] buildPtList(ListNode[] lists){
-        ListNode[] ptList = new ListNode[lists.length];
-        
-        for(int i = 0; i < lists.length; i ++){
-            ListNode n = lists[i];
-            ptList[i] = n;
-        }
-        return ptList;
-    }
 }
