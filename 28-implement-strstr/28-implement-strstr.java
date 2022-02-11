600c1e972ab48e41cc9915912beb822a6fa17472
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-02-10 19:27:16 -0500

    Time: 647 ms (47.74%), Space: 43.9 MB (22.38%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -4,18 +4,20 @@ class Solution {
         if(needle.length()==0) return 0;
         else if(needle.length()>haystack.length()) return -1;
         
+        char[] hay = haystack.toCharArray();
+        char[] need = needle.toCharArray();
         int i = 0;
-        while(i < haystack.length() && haystack.length() - i +1 > needle.length()){ // 
+        while(i < hay.length && hay.length - i +1 > need.length){ // 
             int j = 0;
             
             boolean found = true;
             int update = i;
-            while(i+j < haystack.length() && j < needle.length()){
+            while(i+j < hay.length && j < need.length){
                 
-                if(update == i && haystack.charAt(i+j) == needle.charAt(0)) update = i+j;
+                if(update == i && hay[i+j] == need[0]) update = i+j;
                 
-                char h = haystack.charAt(i+j);
-                char n = needle.charAt(j);
+                char h = hay[i+j];
+                char n = need[j];
                 if(h!=n){
                   found = false;
                   break;
@@ -23,7 +25,7 @@ class Solution {
                 j++;
             }
             
-            if(j==needle.length() && found) return i;
+            if(j==need.length && found) return i;
             if(update!=i) i = update;
             else i++;
         }
@@ -33,6 +35,41 @@ class Solution {
 }
 
 
-
-//  // i0 h // i1 e //i3 l
-//       0123456789
+/*
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length() == 0){
+            return 0;
+        }else if(haystack.length() == 0){
+            return -1;
+        }
+        
+        char[] charHay = haystack.toCharArray();
+        char[] charNdl = needle.toCharArray();
+        
+        for (int i = 0; i < charHay.length; i++){
+            char h = charHay[i];
+            System.out.println("charHay["+i+"]="+charHay[i]);
+            if(i > charHay.length - charNdl.length){
+                return -1;
+            }
+            if(h == charNdl[0]){
+                boolean found = true;
+                for(int j = 0; j < charNdl.length; j++){
+                    System.out.println("found i="+i+" j= "+j+" char[i+j]="+charHay[i+j]+" charNdl[j]="+charNdl[j]);
+                    if(charHay[i+j] != charNdl[j]){
+                        found = false;
+                        break;
+                    }
+                }
+                if(found){
+                    return i;
+                }
+            }
+            
+        }
+        return -1;
+        
+    }
+}
+*/
