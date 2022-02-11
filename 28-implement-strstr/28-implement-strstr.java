502944b8d697e8fc0be3d4d06eb11e7a8303e1e2
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-02-10 19:24:41 -0500

    Time: 602 ms (53.09%), Space: 43.5 MB (23.36%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,38 +1,42 @@
 class Solution {
     public int strStr(String haystack, String needle) {
+        // edge cases:
+        // needle blank, haystack blank
+        // hello, ll
         
-        if(needle.length()==0) return 0;
-        else if(needle.length()>haystack.length()) return -1;
         
-        int i = 0;
-        while(i < haystack.length() && haystack.length() - i +1 > needle.length()){ // 
-            int j = 0;
-            
-            boolean found = true;
-            int update = i;
-            while(i+j < haystack.length() && j < needle.length()){
-                
-                if(update == i && haystack.charAt(i+j) == needle.charAt(0)) update = i+j;
-                
-                char h = haystack.charAt(i+j);
-                char n = needle.charAt(j);
-                if(h!=n){
-                  found = false;
-                  break;
-                } 
-                j++;
+           
+        if(needle.length() == 0){
+            return 0;
+        }else if(haystack.length() == 0){
+            return -1;
+        }
+        
+        char[] charHay = haystack.toCharArray();
+        char[] charNdl = needle.toCharArray();
+        
+        for (int i = 0; i < charHay.length; i++){
+            char h = charHay[i];
+            // System.out.println("charHay["+i+"]="+charHay[i]);
+            if(i > charHay.length - charNdl.length){
+                return -1;
+            }
+            if(h == charNdl[0]){
+                boolean found = true;
+                for(int j = 0; j < charNdl.length; j++){
+                    // System.out.println("found i="+i+" j= "+j+" char[i+j]="+charHay[i+j]+" charNdl[j]="+charNdl[j]);
+                    if(charHay[i+j] != charNdl[j]){
+                        found = false;
+                        break;
+                    }
+                }
+                if(found){
+                    return i;
+                }
             }
             
-            if(j==needle.length() && found) return i;
-            if(update!=i) i = update;
-            else i++;
         }
         return -1;
         
     }
 }
-
-
-
-// hellowhatthefuck // i0 h // i1 e //i3 l
-//       0123456789
