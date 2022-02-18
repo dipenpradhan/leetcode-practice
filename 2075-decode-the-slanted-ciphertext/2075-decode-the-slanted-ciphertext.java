d29018f51551a82400437ec69bef97c0db4248ed
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-02-17 22:15:05 -0500

    Time: 47 ms (67.05%), Space: 77.9 MB (43.75%) - LeetHub

---
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
@@ -0,0 +1,30 @@
+class Solution {
+    private static int ROWS, COLS;
+    public String decodeCiphertext(String encodedText, int rows) {
+        ROWS = rows;
+        COLS = (encodedText.length()/rows) + (encodedText.length()%rows>0?1:0);
+        
+        StringBuilder res = new StringBuilder();
+        
+        for(int c = 0; c < COLS; c++){
+            diag(encodedText, 0, c, res);
+        }
+        int l = res.length()-1;
+        while(l>=0){
+            if(res.charAt(l)!=' ') break;
+            l--;
+        }
+        res.setLength(l+1);
+        return res.toString();
+    }
+    
+    private void diag(String enc, int r, int c, StringBuilder res){
+        
+        if(r>=ROWS || c>=COLS) return;
+        
+        int pos = r*COLS + c;
+        
+        res.append(enc.charAt(pos));
+        diag(enc, ++r, ++c, res);
+    }
+}
