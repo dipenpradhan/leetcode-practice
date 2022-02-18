1298246689a68129656dca86b30cf71fdbac9aff
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-02-17 22:53:07 -0500

    Time: 48 ms (5.14%), Space: 44 MB (5.98%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -1,40 +1,40 @@
 class Solution {
     public int myAtoi(String s) {
-        Pair<Integer,String> num = parseNum(s);
-        if(num.getValue().length()==0) return 0;
-        long res = 0;
-        for(char c : num.getValue().toCharArray()){
-            res*=10;
-            res += num.getKey()*Character.getNumericValue(c);
-            if(res >= Integer.MAX_VALUE){
-                res = Integer.MAX_VALUE;  
-                break;
-            }else if (res <= Integer.MIN_VALUE){
-                res = Integer.MIN_VALUE;
-                break;
-            }
-        }
         
-        return (int)res;
+        return parseNum(s);
     }
     
-    private Pair<Integer, String> parseNum(String s){
-        int sign = 0;
-        StringBuilder res = new StringBuilder();
-        
+    private int parseNum(String s){
+        int sign = 0, res = 0;
+        boolean seen = false;
         for(char c : s.toCharArray()){
             
             if(Character.isDigit(c)){
+                if(!seen) seen = true;
                 sign = sign==0?1:sign;
-                res.append(c);
+                if(sign<0 && res>=0) res = -res;
+                
+                if(sign>0 && res > Integer.MAX_VALUE/10 || (res == Integer.MAX_VALUE/10 && Character.getNumericValue(c)>7)){
+                    return Integer.MAX_VALUE;
+                }else if(sign<0 && res < Integer.MIN_VALUE/10 || (res == Integer.MIN_VALUE/10 && Character.getNumericValue(c)>8)){
+                    return Integer.MIN_VALUE;
+                }
+                                                System.out.println("seen and res= "+res+" c="+Character.getNumericValue(c));
+
+                res*=10;
+                res += sign*Character.getNumericValue(c);
+
             }else if(sign==0 && (c == '+' || c == '-')){
+                System.out.println("sign==0 and sign found "+c);
                 sign = c == '+'? 1 : -1;
-            }else if(sign!=0 || res.length() != 0 || c != ' '){
+            }else if(sign!=0 || seen || c != ' '){
                 break;
+            }else{
+                if(seen) break;
             }
             
         }
         
-        return new Pair<Integer, String>(sign, res.toString());
+        return res;
     }
 }
