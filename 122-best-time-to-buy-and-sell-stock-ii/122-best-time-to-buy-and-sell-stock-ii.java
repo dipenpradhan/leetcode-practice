329e5bb069293f92770a198c661a88baad503bc1
Dipen Pradhan <dipenpradhan13@gmail.com>
2022-03-03 23:50:02 -0500

    Time: 2 ms (35.54%), Space: 44.5 MB (16.99%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxProfit(int[] prices) {
         int maxP = 0;
         
-        for(int i = 0; i < prices.length; i++){
+        for(int i = 0; i < prices.length-1; i++){
             int p = prices[i];
             int p2 = i+1<prices.length?prices[i+1]:0;
             
